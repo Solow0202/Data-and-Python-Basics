dd53c30a8d2784abd2c0cd3a28d21371e3ab07d1
Methusela Solomon <solomon.m0202@gmail.com>
2026-08-26 16:10:00 +0300

    Update 02_data_types.py

--- Python Basics/Python Basics/02_data_types.py
+++ Python Basics/Python Basics/02_data_types.py
@@ -24,3 +24,17 @@ print(type(age))
 print(type(height))
 print(type(is_student))
 
+favorite_club = "Manchester United"
+goals_scored = 50
+current_gpa = 1.8
+enjoying_python = True
+
+print(favorite_club)
+print(goals_scored)
+print(current_gpa)
+print(enjoying_python)
+
+print(type(favorite_club))
+print(type(goals_scored))
+print(type(current_gpa))
+print(type(enjoying_python))
