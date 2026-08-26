25b027a26863e449bbea91a07f609d4316535050
Methusela Solomon <solomon.m0202@gmail.com>
2026-08-26 16:30:29 +0300

    Create 03_user_input.py

---
+++ Python Basics/Python Basics/Python Basics/03_user_input.py
@@ -0,0 +1,10 @@
+# Create a program that asks the user for their name, their age, and their favorite football club.
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+favorite_club = input("What is your favorite football club? ")
+
+print(name)
+print(age)
+print(favorite_club)
+
+print(f"Hello {name}! You are {age} years old and you support {favorite_club}.")
