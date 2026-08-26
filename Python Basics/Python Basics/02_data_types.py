5813500f0208359e58e571bf94e1d8fa05a91d12
Methusela Solomon <solomon.m0202@gmail.com>
2026-08-26 16:01:10 +0300

    Create 02_data_types.py

---
+++ Python Basics/Python Basics/02_data_types.py
@@ -0,0 +1,26 @@
+# Python Data Types
+
+# String
+name = "Solow"
+
+# Integer
+age = 20
+
+# Float
+height = 1.75
+
+# Boolean
+is_student = True
+
+# Display the values
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+# Display the data types
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
