6e6a43407cf89675c4c69256c4f8c3f886afe000
Methusela Solomon <solomon.m0202@gmail.com>
2026-09-01 20:24:10 +0300

    Update 04_operators.py

--- Python Basics/04_operators.py
+++ Python Basics/04_operators.py
@@ -7,3 +7,20 @@ print(a/b)
 print(a//b)
 print(a%b)
 print(a**b)
+
+a=20
+b=6
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+# Logical operators
+a = 20
+b = 6
+print(a > 10 and b > 5)
+print(a < 10 or b < 10)
+print(a > 10 and b > 10)
+print(a not < 10)
